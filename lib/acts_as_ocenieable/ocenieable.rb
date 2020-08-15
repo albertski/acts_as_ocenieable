825400bf991a72b2cf35064b2868af26d791951d
Albert Jankowski <albert@xenomedia.com>
2020-08-15 14:25:23 -0500

    Refactoring build_ocenies.

--- lib/acts_as_ocenieable/ocenieable.rb
+++ lib/acts_as_ocenieable/ocenieable.rb
@@ -12,6 +12,36 @@ module ActsAsOcenieable
 
         before_save :add_default_ocenie_values
       end
+
+      private
+
+      def build_ocenies_based_on_options
+        self.acts_as_ocenieable_options[:rating_types].each do |rating_type_category, rating_types|
+          rating_types.each do |rating_type|
+            has_rating = false
+
+            self.ocenies.each do |ocenie|
+              has_rating = true if ocenie.has_category_and_type?(rating_type_category, rating_type)
+            end
+
+            self.ocenies.build rating_type_category: rating_type_category,
+                                       rating_type: rating_type,
+                                       max_rating: self.acts_as_ocenieable_options[:max_rating] if !has_rating
+          end
+        end
+      end
+
+      def remove_ocenies_not_in_options
+        self.ocenies = self.ocenies.select do |current_ocenie|
+          reject = false
+          self.acts_as_ocenieable_options[:rating_types].each do |rating_type_category, rating_types|
+            rating_types.each do |rating_type|
+              reject = true if current_ocenie.has_category_and_type?(rating_type_category, rating_type)
+            end
+          end
+          reject
+        end
+      end
     end
 
     def add_default_ocenie_values
@@ -21,30 +51,8 @@ module ActsAsOcenieable
     end
 
     def build_ocenies
-      self.acts_as_ocenieable_options[:rating_types].each do |rating_type_category, rating_types|
-        rating_types.each do |rating_type|
-          has_rating = false
-
-          self.ocenies.each do |ocenie|
-            has_rating = true if ocenie.has_category_and_type?(rating_type_category, rating_type)
-          end
-
-          self.ocenies.build rating_type_category: rating_type_category,
-                                     rating_type: rating_type,
-                                     max_rating: self.acts_as_ocenieable_options[:max_rating] if !has_rating
-        end
-      end
-
-      self.ocenies = self.ocenies.select do |current_ocenie|
-        reject = false
-        self.acts_as_ocenieable_options[:rating_types].each do |rating_type_category, rating_types|
-          rating_types.each do |rating_type|
-            reject = true if current_ocenie.has_category_and_type?(rating_type_category, rating_type)
-          end
-        end
-        reject
-      end
-
+      build_ocenies_based_on_options
+      remove_ocenies_not_in_options
       self.ocenies
     end
   end
